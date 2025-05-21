3257f1616f0fe7aa72f6ac155fd629d5ee21d97f
Zdeňka Kameníková <111301275+Alita-oss@users.noreply.github.com>
2025-05-21 13:34:39 +0200

    feat: frontend basic layout

--- app.vue
+++ app.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
+  <div class="layout">
     Hi 
+    <p class="hello">Hello</p>
+    <h2 class="random">hesixdfgdj</h2>
   </div>
 </template>
 
@@ -16,5 +18,23 @@
     const data = await res.json();
     console.log('Response:', data); 
   });
-
 </script>
+
+<style scoped lang="scss">
+  .layout {
+    max-width: 1200px;
+    margin: 0 auto;
+  }
+
+  .hello {
+    @include typography.font(body, xl)
+  }
+
+  .random {
+    color: lightblue;
+
+    @include functions.device(tablet) {
+      color: pink;
+    }
+  }
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,23 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
+  css: ['@/assets/styles/main.scss'],
   modules: ['@nuxt/eslint', '@nuxt/test-utils', 'nuxt-mongoose'],
 
   runtimeConfig: {
     MONGODB_CONNECTION: ''
-  }
-})
+  },
+
+  vite: {
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData: `
+                        @use "@/assets/styles/functions.scss";
+                        @use "@/assets/styles/typography.scss";
+                    `,
+                },
+            },
+        },
+    },
+});
