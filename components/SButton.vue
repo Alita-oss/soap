6ee96b65af7a0a85f9fd4e88e704a97620301c57
Zdeňka Kameníková <111301275+Alita-oss@users.noreply.github.com>
2025-05-26 15:09:22 +0200

    feat(SOAP-13): submit button (#11)
    * feat: submit button
    
    * feat: button
    
    * fix: button

---
+++ components/SButton.vue
@@ -0,0 +1,39 @@
+<template>
+    <button class="s-button">
+        <slot />
+    </button>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.s-button {
+    border-radius: 20px;
+    border: 1px solid #000;
+    cursor: pointer;
+    width: 100%;
+    padding: 0.5rem 1.5rem;
+
+    @include typography.font(body, m);
+
+    &:hover {
+        background-color: var(--color-btn-bg-hover);
+    }
+
+    &:active {
+        background-color: var(--color-btn-bg-active);
+    }
+
+    &:disabled {
+        background-color: var(--color-btn-bg-disabled);
+        color: #aaa;
+        border-color: #ccc;
+        cursor: not-allowed;
+
+        &:hover {
+            background-color: var(--color-btn-bg-disabled);
+            color: #aaa;
+        }
+    }
+}
+</style>
