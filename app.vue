c20fcfab308ebeaa726a796d1b28886faf069886
Zdeňka Kameníková <111301275+Alita-oss@users.noreply.github.com>
2025-05-27 13:42:29 +0200

    feat: select component

--- app.vue
+++ app.vue
@@ -1,8 +1,32 @@
 <template>
-    <div class="layout"></div>
+    <div class="layout">
+        <!--<div>
+            <SInput></SInput>
+            <SSelect v-model="selectedUnit" :options="unitOptions">
+                <template #label>Choose Unit</template>
+            </SSelect>
+            <p>{{ selectedUnit }}</p>
+            <SInput></SInput>
+            <SButton>Submit</SButton>
+        </div>-->
+    </div>
 </template>
 
 <script setup lang="ts">
+/*import SSelect from './components/SSelect.vue';
+import SInput from './components/SInput.vue';
+import SButton from './components/SButton.vue';
+import type { Unit } from '~/types/unit';
+
+const unitOptions: { value: Unit; text: string }[] = [
+    { value: 'g', text: 'grams' },
+    { value: 'oz', text: 'ounces' },
+    { value: 'ml', text: 'milliliters' },
+    { value: 'tsp', text: 'teaspoons' },
+    { value: 'tbsp', text: 'tablespoons' },
+];
+const selectedUnit = ref<Unit>('g');*/
+
 onMounted(async () => {
     /*const res = await fetch('/api/recipe', {
         method: 'GET',
---
+++ components/SSelect.vue
@@ -0,0 +1,38 @@
+<template>
+    <label>
+        <span>
+            <slot name="label" />
+        </span>
+        <select
+            name="select"
+            class="s-select"
+            :value="modelValue"
+            @input="$emit('update:modelValue', $event.target.value)"
+        >
+            <option v-for="option in options" :key="option.value" :value="option.value">
+                {{ option.text }}
+            </option>
+        </select>
+    </label>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+    modelValue: string;
+    options: { value: string; text: string }[];
+}>();
+
+defineEmits(['update:modelValue']);
+</script>
+
+<style scoped lang="scss">
+.s-select {
+    border: solid 1px #000;
+    border-radius: 20px;
+    padding: 0.5rem 1.5rem;
+    width: 100%;
+    height: 49.6px;
+
+    @include typography.font(body, m);
+}
+</style>
