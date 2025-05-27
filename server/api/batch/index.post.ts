799b2232e18362f83ca19d3e0a2787278806b030
Zdeňka Kameníková <111301275+Alita-oss@users.noreply.github.com>
2025-05-27 10:37:41 +0200

    feat(SOAP-15): batch type, schema and endpoints (#12)
    * feat: batch type, schema and endpoints
    
    * fix: names in post requests

--- server/api/batch/index.post.ts
+++ server/api/batch/index.post.ts
@@ -1,30 +1,30 @@
-import status from 'http-status';
-import { Recipe } from '~/server/models/recipe';
-import { handleCatchError } from '~/server/utils/api';
+import { Batch } from '~/server/models/batch';
 import { ErrorPrefix, ErrorTypes } from '~/types/error';
+import status from 'http-status';
+import { handleCatchError } from '~/server/utils/api';
 
 export default defineEventHandler(async (event) => {
     try {
         const body = await readBody(event);
 
-        if (!body.name || !Array.isArray(body.ingredients) || body.ingredients.length === 0) {
+        if (!body.name || !body.recipe || !body.createdDate || !body.weightIns) {
             throw new Error(ErrorTypes[status.PRECONDITION_FAILED]);
         }
 
-        const existingRecipe = await Recipe.findOne({
+        const existingBatch = await Batch.findOne({
             name: body.name,
         });
 
-        if (existingRecipe) {
+        if (existingBatch) {
             throw new Error(ErrorTypes[status.CONFLICT]);
         }
 
-        const recipe = await Recipe.create(body);
+        const batch = await Batch.create(body);
         return {
-            recipe,
+            batch,
             statusCode: status.OK,
         };
     } catch (err) {
-        handleCatchError(`${ErrorPrefix.API} Error creating new recipe`, err);
+        handleCatchError(`${ErrorPrefix.API} Error creating new batch`, err);
     }
 });
